65c7cd0359a5968329aa62a8f11dc5e2081b0553
James <j.a.pratt@outlook.com>
2021-04-17 20:46:36 +0100

    ForecastSummaries validated props

--- src/components/ForecastSummaries.js
+++ src/components/ForecastSummaries.js
@@ -1,6 +1,33 @@
 import React from "react";
+import PropTypes from "prop-types";
+import ForecastSummary from "./ForecastSummary";
 
-const ForecastSummaries = () => {
-  return <div className="forecast-summaries" />;
+const ForecastSummaries = ({ forecasts }) => (
+  <div className="forecast-summaries">
+    {forecasts.map((forecast) => (
+      <ForecastSummary
+        key={forecast.date}
+        date={forecast.date}
+        description={forecast.description}
+        icon={forecast.icon}
+        temperature={forecast.temperature}
+      />
+    ))}
+  </div>
+);
+
+ForecastSummaries.propTypes = {
+  forecasts: PropTypes.arrayOf(
+    PropTypes.shape({
+      data: PropTypes.number,
+      description: PropTypes.string,
+      icon: PropTypes.string,
+      temperature: PropTypes.shape({
+        max: PropTypes.number,
+        min: PropTypes.number,
+      }),
+    })
+  ).isRequired,
 };
+
 export default ForecastSummaries;
