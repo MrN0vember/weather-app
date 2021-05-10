44936d8fede04286ceb377aca8e9e657fb27dfce
James <j.a.pratt@outlook.com>
2021-05-10 16:37:18 +0100

    app tests fixed snapshots updated

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,34 +2,8 @@ import { render, screen } from '@testing-library/react';
 import App from '../components/App';
 
 describe("App", () => {
-  it("renders city and country", () => {
-    const validProps = {
-      location: {
-        city: "Manchester",
-        country: "United Kingdom",
-      },
-      forecasts: [
-        {
-          date: 1111111,
-          description: "Mock description",
-          humidity: 50,
-          icon: "800",
-          temperature: {
-            min: 9,
-            max: 17,
-          },
-          wind: {
-            speed: 7,
-            direction: "e",
-          },
-        },
-      ],
-    };
-
-    render(
-      <App forecasts={validProps.forecasts} location={validProps.location} />
-    );
-    const copy = screen.getByText(/Manchester, United Kingdom/i);
-    expect(copy).toBeInTheDocument();
+  it("renders correctly", () => {
+    const { asFragment } = render(<App />);
+    expect(asFragment()).toMatchSnapshot();
   });
 });
